433de1222c415b469a080c50d48a41336db6b227
FinnCollins2 <finncollins2@gmail.com>
2024-10-15 22:19:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// @ts-nocheck
+// console.log("Start");
+
+$(document).ready(function(){       
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+  
+    var top = $('#top');
+    var topOffset = top.offset();
+  
+    if (startchange.length){
+    $(document).scroll(function() { 
+  
+      // console.log(offset);
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > topOffset.top) {
+        $("#home").css('background-color', 'teal');
+  
+        $(".nav").css('color', 'black');
+        $("<style>#logo::after{border-color:black}</style>" ).appendTo("head");
+  
+        // $(".nav").css('color', 'white');
+        // $("<style>#logo::after{border-color: orange}</style>" ).appendTo("head");
+  
+        $("h1").css('color', 'white');
+      } else {
+        $("#home").css('background-color', 'white');
+  
+        $('.nav').css('color', 'black');
+        $("<style>#logo::after{border-color: teal}</style>" ).appendTo("head");
+        
+        // $('.nav').css('color', 'black');
+        // $("<style>#logo::after{border-color: blue}</style>" ).appendTo("head");
+        $("h1").css('color', 'black');
+      }
+  
+      // if(scroll_start > offset.top) {
+      //     $(".nav").css('color', 'orange');
+      //     $("<style>#logo::after{border-color: orange}</style>" ).appendTo("head");
+      // } else {
+      //     $('.nav').css('color', 'black');
+      //     $("<style>#logo::after{border-color: blue}</style>" ).appendTo("head");
+      // }
+  
+  
+  
+      });
+     }
+  });
+  
+  
+  
+  
